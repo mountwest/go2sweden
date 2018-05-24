568fab19a2d53c3e1aa319e8e33c07f548f87c19
AppsDaniel <apps.DanielLarsson@gmail.com>
2018-05-24 16:09:01 +0200

    changed the table to angular. Get data from API true Angular.

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,32 +1,77 @@
 $(document).ready(function () {
 
-    $('#apisearch').on('click', function () {
+//     $('#apisearch').on('click', function () {
 
-        var data =
-            {
-                fromDestination: $('#fromDestination').val().trim(),
-                toDestination: $('#toDestination').val().trim(),
-            };
+        
 
-        if (data.fromDestination !== '' && data.toDestination !== '') {
-            $.ajax(
-                {
-                    url: 'http://free.rome2rio.com/api/1.4/json/Search?key=S2Q8spaR&oName=' + data.fromDestination + '&dName=' + data.toDestination + '&noRideshare',
-                    type: 'GET',
-                    dataType: "json",
-                    processData: false,
-                    cache: false
-                }).fail(function (jqXHR, textStatus, errorThrown) {
-                    alert("Something went wrong (" + textStatus + ")");
-                }).done(function (result, textStatus, jqXHR) {
-                    let names = result.routes;
-                    $("#names").empty();
-                    return names.map(function (city) {
-                        $('#names > tbody:last-child').append('<tr><td>' + city.name + "</td><td>" + parseInt(city.totalDuration/60) + "h " + (city.totalDuration%60) + "min" + "</td><td>" + city.indicativePrices[0].price + " " + city.indicativePrices[0].currency + '</td></tr>' );
-                    })
-                });
-        }
-    });
+//         var data =
+//             {
+//                 fromDestination: $('#fromDestination').val().trim(),
+//                 toDestination: $('#toDestination').val().trim(),
+//             };
+
+//         if (data.fromDestination !== '' && data.toDestination !== '') {
+//             $.ajax(
+//                 {
+//                     url: 'http://free.rome2rio.com/api/1.4/json/Search?key=S2Q8spaR&oName=' + data.fromDestination + '&dName=' + data.toDestination + '&noRideshare',
+//                     type: 'GET',
+//                     dataType: "json",
+//                     processData: false,
+//                     cache: false
+//                 }).fail(function (jqXHR, textStatus, errorThrown) {
+//                     alert("Something went wrong (" + textStatus + ")");
+//                 }).done(function (result, textStatus, jqXHR) {
+//                     let names = result.routes;
+                    
+//                     $("#names").empty();
+//                     return names.map(function (city) {
+
+//                     // var i, j = "";
+                        
+//                     // for (i = 0; i < city[i].segments; i++) {
+
+//                     //     for(j= 0; j < segments[j].lenght; j++){
+                                 
+//                     //     }
+//                     // }
+
+//                     var transNumber = city.segments[0].vehicle;
+                
+//                     for (j = 0; j < names.length; j++){
+//                         // for (x = 0; x < city.segments.length; x++){
+//                     $('#names > tbody:last-child').append('<tr><td>' + names[j].name + "</td><td>" + parseInt(city.totalDuration/60) + "h " + (city.totalDuration%60) + "min" + "</td><td>" + city.indicativePrices[0].price + " " + city.indicativePrices[0].currency + "</td><td>" + result.vehicles[j].name + '</td></tr>');
+                    
+                
+//                 }})
+                
+//                 });
+//         }
+
+
+//         if (data.fromDestination !== '' && data.toDestination !== '') {
+//             $.ajax(
+//                 {
+//                     url: 'http://free.rome2rio.com/api/1.4/json/Search?key=S2Q8spaR&oName=' + data.fromDestination + '&dName=' + data.toDestination + '&noRideshare',
+//                     type: 'GET',
+//                     dataType: "json",
+//                     processData: false,
+//                     cache: false
+//                 }).fail(function (jqXHR, textStatus, errorThrown) {
+//                     alert("Something went wrong (" + textStatus + ")");
+//                 }).done(function (transportresult, textStatus, jqXHR) {
+                    
+//                     let transportmode = transportresult.routes;
+//                     // let transportmode = transportresult.routes;
+//                     $("#transportmode").empty();
+//                     // $('#transportmode > tbody:last-child').append('<tr><td>' + transportresult.vehicles[0].name + '</td><td>');
+//                 return transportmode.map(function (tmode) {
+                  
+//                // $('#transportmode > tbody:last-child').append('<tr><td>' + transportresult.vehicles[0].name + '</td><td>');
+//                 $('#transportmode > tbody:last-child').append('<tr><td>' + tmode.segments[0].vehicle + '</td><td>');
+//                  })
+//                 });
+//         }
+//     });
 
     $(".scroll").click(function (event) {
         event.preventDefault();
