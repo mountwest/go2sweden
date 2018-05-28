ac49b7479c314c0b89a4ba2090a5c7e1b55c1a20
AppsDaniel <apps.DanielLarsson@gmail.com>
2018-05-28 10:52:45 +0200

    added images for transport (switch), also changes button ng-show/hide for start/stop

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -2,16 +2,16 @@ var myapp = angular.module("myapp", []);
 
 myapp.controller("ApiController", function($scope, $http){
 // Har en funktion redo för att gömma och vissa information
-//     $scope.going = false;
+    $scope.going = false;
 
-// $scope.goEvent = function(){
-//    $scope.going = !$scope.going;
-//    if($scope.going){  
-//        $scope.go();
-//    }else{
-//        $scope.stop();
-//    }      
-// }
+$scope.goEvent = function(){
+   $scope.going = !$scope.going;
+   if($scope.going){  
+       $scope.go();
+   }else{
+       $scope.stop();
+   }      
+}
 
 
     $scope.getData = function(){
@@ -26,12 +26,7 @@ myapp.controller("ApiController", function($scope, $http){
             $scope.routes = response.data.routes;
             $scope.vehicles = response.data.vehicles;
 
-            $scope.daniel = "buffered";
-            
-
             $scope.timeChange = function(timeinmin){
-
-
 
                 let hours = parseInt(timeinmin/60);
                 let minutes = timeinmin%60;
@@ -39,9 +34,49 @@ myapp.controller("ApiController", function($scope, $http){
                 let timeToDestination = hours + "h "+ minutes + "min";
 
                 return timeToDestination;
-
-                
             }
+
+            $scope.setIcon = function(inComingNumber){
+
+                let icon =" ";
+                
+                switch (inComingNumber){
+                    case inComingNumber = 0 : 
+                    icon = "fas fa-train fa-2x";
+                    break;
+                    case inComingNumber = 1 :
+                    icon = "fas fa-plane fa-2x";
+                    break;
+                    case inComingNumber = 2 :
+                    icon = "fas fa-bus fa-2x";
+                    break;
+                    case inComingNumber = 3 :
+                    icon = "fas fa-bus fa-2x";
+                    break;
+                    case inComingNumber = 4 :
+                    icon = "fas fa-car fa-2x";
+                    break;
+                    case inComingNumber = 5 :
+                    icon = "fas fa-camera-retro fa-2x";
+                    break;
+                    case inComingNumber = 6 :
+                    icon = "fas fa-camera-retro fa-2x";
+                    break;
+                    case inComingNumber = 7 :
+                    icon = "fas fa-camera-retro fa-2x";
+                    break;
+                    default: ;
+                    icon = "ooops "  
+                    break;
+                    
+                }
+
+               
+                
+                return icon;
+            }
+
+
 
           
            
