3b606c8d63bf683c446621ce202101732ec5e8a3
AppsDaniel <apps.DanielLarsson@gmail.com>
2018-05-30 14:21:03 +0200

    added automatic city in input

--- wwwroot/js/table.js
+++ wwwroot/js/table.js
@@ -38,6 +38,10 @@ $scope.tripShow = function(){
 
             $scope.daniel = "buffered";
             
+            $scope.toDest = function(){
+                let toDesti = " to " +  to;
+                return toDesti;
+            }
 
             $scope.timeChange = function(timeinmin){
 
@@ -129,3 +133,22 @@ $scope.tripShow = function(){
     }
 
 })
+
+myapp.directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());                
+                });
+            });
+        }
+    };
+});
